aa75a2453fe461be7168413b3b517588dd676e04
Cristian San <cristiansan@gmail.com>
2024-12-11 12:30:15 -0300

    Update TP3TokenB.sol
    b

--- TP3TokenB.sol
+++ TP3TokenB.sol
@@ -1,1 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.5;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract TokenB is ERC20 {
+    constructor() ERC20("TokenB", "TKB") {
+        _mint(msg.sender, 1000000 * 10 ** decimals()); // Mint 1,000,000 TKB to the deployer
+    }
+}
